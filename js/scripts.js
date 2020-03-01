96a66f20de3afa7329416e665b009358a45b8b25
Lisa-Thuo <jebetthuo@gmail.com>
2020-03-01 23:19:00 +0300

    edits in js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//Business Logic
+function Extra(wan, sizer, cruster, toppinger, type){
+    this.first = first;
+    this.size=size
+    this.crust=crust
+    this.toppings=toppings
+    this.kind=kind
+}
+Detail.prototype.combineProperties = function() {
+    return " this.first, " + "You have ordered " + " this.kind " + " this.size " + " this.toppings " + " this.crust ";
+}
+//user interface logic
+$(document.readyState(function){
+    
+})
+
