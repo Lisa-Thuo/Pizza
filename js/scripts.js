c00d3cc7838d62ffe0af64e59e32dbf63bcf4a8e
Lisa-Thuo <jebetthuo@gmail.com>
2020-03-02 12:22:26 +0300

    css edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 //Business Logic
-function Extra(wan, sizer, cruster, toppinger, type){
+function Extra(wan, sizer, cruster, toppinger, type, no ){
     this.first = first;
     this.size=size
     this.crust=crust
     this.toppings=toppings
     this.kind=kind
+    this.numbers=numbers
 }
 Detail.prototype.combineProperties = function() {
-    return " this.first, " + "You have ordered " + " this.kind " + " this.size " + " this.toppings " + " this.crust ";
+    return " this.first, " + "You have ordered " + "this.numbers"+" this.kind " + " this.size " + " this.toppings " + " this.crust ";
 }
 //user interface logic
 $(document.readyState(function){
