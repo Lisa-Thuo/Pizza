c7e0d0eefbe299dcceadcea161a1bff037d58b33
Lisa-Thuo <jebetthuo@gmail.com>
2020-03-03 21:13:07 +0300

    edits in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,6 @@
 //Business Logic
 
-    function Extra(kind, size, crust, toppings, numbers, delivery){
-        this.kind=kind,
-        this.size=size,
-        this.crust=crust,
-        this.toppings=toppings,
-        this.numbers=numbers,
-        this.delivery=del
-
-        Extra.prototype.combineProperties = function() {
-            return " this.first, " + "You have ordered " + "this.numbers"+" this.kind " + " this.size " + " this.toppings " + " this.crust ";
-        
-        $(document).ready(function(){
+    function myFunction(kind, size, crust, toppings, numbers, delivery){
         this.crusts={
             "crispy":100,
             "stuffed":100,
@@ -21,36 +10,47 @@
             "cheese":50,
             "sauce":50,
             "olives":50,
-            "bay":50
+            "bay":50,
         }
         this.kind={
-            bacon:{"small": 500, "medium":800, "large":1200}
+            "Chicken Barbeque":{"small":500, "medium":800,"large":1200},
+            "Hawaiian":{"small":500, "medium":800,"large":1200},
+            "Vegetarian":{"small":500, "medium":800, "large":1200},
+            "Bacon":{"small": 500, "medium":800, "large":1200},
+            "Gluten Free":{"small": 500, "medium":800, "large":1200},
+
     
         }
         this.delivery={
             "yes": 200,
             "no":0
         }
-        this.numbers=numbers
+        //User Interface
+        $("#orderPizza").click (function(){
+            var type = $("#type").val;
+            var crust = $('#crust').val;
+            var toppings = $('#toppings').val;
+            var del = $("#del").val;
+            var cost = new Extra(type, crust, toppings, del);
+            console.log(cost);
+        });
+        
+    
+    this.numbers=numbers
         this.price=(this.kind[kind][size] + this.toppings[toppings] + this.crusts[crust] + this.delivery[delivery]) * numbers;
+        Extra.prototype.combineProperties = function() {
+            return [" this.first, "+ "You have ordered " + "this.numbers"+" this.kind " + " this.size " + " this.toppings " + " this.crust "*"numbers"]
+        }
+        if(del="yes"){
+            return ["Your total will be increased by two hundred because of delivery charges"]
+            
+        }
+        else if(del="no"){
+        return [" this.first, "+ "You have ordered " + "this.numbers"+" this.kind " + " this.size " + " this.toppings " + " this.crust "*"numbers"]
+    
     }
     
     
-    };
-    $("#orderPizza").click (function(){
-        var type = $("#type").val;
-        var crust = $('#crust').val;
-        var toppings = $('#toppings').val;
-        var del = $("#del").val;
-        var cost = new Extra(type, crust, toppings, del);
-        console.log(cost);
-    });
-    alert('first'+ "You have ordered"+ 'number'+ 'pizza' + 'for'+ '*number')
-    //user interface logic
-    // $(document).ready(function(){
-    //   var  first=parseInt(document.getElementByClassName("first").value);
-    //   var  last=parseInt(document.getElementByClassName("last").value);
-    //  }) 
-})
+   
 
 
